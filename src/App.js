06a171410dc42383ecdcc19e481dca97ae84ea3c
Kartikeya <kartikeya.jain.delhi@gmail.com>
2022-06-25 17:17:30 +0530

    fixed env var issue

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import './App.css'
 import Sidebar from './components/Sidebar'
 import Chat from './components/Chat'
 import { login, logout, selectUser } from './features/userSlice'
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux'
 import Login from './components/Login.js'
 import { auth } from './firebase'
 
 function App() {
-
   const user = useSelector(selectUser)
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
-      if(authUser) {
-        dispatch(login({
-          uid: authUser.uid,
-          photo: authUser.photoURL,
-          displayName: authUser.displayName,
-          email: authUser.email,
-        }))
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            displayName: authUser.displayName,
+            email: authUser.email,
+          })
+        )
         console.log(authUser)
       } else {
         dispatch(logout())
@@ -28,15 +29,16 @@ function App() {
   }, [dispatch])
   return (
     <div className='App'>
-      {
-        user ? (<>
+      {user ? (
+        <>
           <Sidebar />
-          <Chat /> 
-        </>) : (
+          <Chat />
+        </>
+      ) : (
         <Login />
       )}
     </div>
-  );
+  )
 }
 
 export default App
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -9,7 +8,7 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDTbN6eHu26AXROOsMeJqw0dPtfiygzvbE",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "beyontalk.firebaseapp.com",
   projectId: "beyontalk",
   storageBucket: "beyontalk.appspot.com",
@@ -20,7 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
